3c9312bc31568ed758b0e565f5b47c40daf4dade
luckyman0026 <your_github_email_here>
2025-12-11 15:59:35 -0500

    fix:helloworld.js

--- blocks/helloworld/helloworld.js
+++ blocks/helloworld/helloworld.js
@@ -1,12 +1,3 @@
-// /blocks/helloworld/helloworld.js
 export default function decorate(block) {
-  // Clear any existing content
-  block.textContent = '';
-
-  // Create a simple greeting element
-  const greeting = document.createElement('div');
-  greeting.textContent = 'Hello World from a Franklin block!';
-
-  // Add it into the block
-  block.appendChild(greeting);
+  block.textContent = "Hello World from a Franklin block!";
 }
